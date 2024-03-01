efda9fee9af0872c96ea5135d232c94867391d53
marianoherrerasolution <marianoherrerasolution@gmail.com>
2024-03-01 16:54:00 +0700

    [widget] modified javascript widget with remote acction

--- frontend/public/widget.js
+++ frontend/public/widget.js
@@ -10,11 +10,39 @@
     }
     var token = String(source.match(/token=.*?($|&)/gi)).replace(/(token=)|(&$)/gi, "")
     var offset = new Date().getTimezoneOffset()
+
+    const successCallback = function(resp) {
+      var message = `You are ${resp.data.access}ed to access this page`
+      if (resp.data.action == "nothing") {
+        return
+      }
+      if (resp.data.action == "alert") {
+        alert(resp.data.message || message)
+        return
+      }
+      if (resp.data.action == "close") {
+        window.close()
+        return
+      }
+      if (resp.data.action == "alert_close") {
+        alert(resp.data.message || message)
+        window.close()
+        return
+      }
+      if (resp.data.action == "alert_redirect") {
+        alert(resp.data.message || message)
+        window.location = resp.data.redirect
+        return
+      }
+      if (resp.data.action == "redirect") {
+        window.location = resp.data.redirect
+        return
+      }
+    }
+
     const endpoint = "http://localhost:4000/scan?token=${token}&offset=${offset}"
     fetch(endpoint).
-    then(function(response) {
-      console.log(response)
-    }).
+    then(successCallback).
     catch(function(err) {
       if (err.status == 401) {
         alert("Geonius token is invalid")
@@ -29,17 +57,8 @@
                 latitude,
                 longitude
               } = position.coords;
-              fetch(`${endpoint}&latitude=${latitude}&longitude=${longitude}`).
-              then(function(resp) {
-                console.log(resp)
-              }).
-              catch({
-                if (err.status == 403) {
-                  alert("You are not allowed to access this page")
-                  return
-                }
-              })
-
+              fetch(`${endpoint}&latitude=${latitude}&longitude=${longitude}&offset=${offset}`).
+              then(successCallback)
             },
             (error) => {
               console.error('Error getting user location:', error);
